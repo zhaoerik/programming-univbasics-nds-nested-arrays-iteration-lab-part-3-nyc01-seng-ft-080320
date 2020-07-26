8b155d21757e6b462236f5a14428f32d5cd4861d
Erik Zhao <erikzhao98@gmail.com>
2020-07-26 15:16:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if element_index.class == "Integer"
+      if src[row_index][element_index].class == "Integer"
         output << ""
       else
         output << src[row_index][element_index]
